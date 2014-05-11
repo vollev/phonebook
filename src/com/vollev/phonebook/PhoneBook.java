fd25f3d3211537e24e0fecf144694e48e98033a5
vollev <vollev@mail.ru>
2014-05-11 21:20:13 +0400

    Вариант 1

--- src/com/vollev/phonebook/PhoneBook.java
+++ src/com/vollev/phonebook/PhoneBook.java
@@ -3,5 +3,31 @@ package com.vollev.phonebook;
 /**
  * Created by vollev on 11.05.2014.
  */
-public class PhoneBook {
+public class PhoneBook { //создаем класс
+    private int number ; // объявление целочисленной переменной number
+    private String firstName, lastName, phone ; // объявление класса String и его экземпляров
+
+    public int getNumber () {
+        return number;          // значению переменной getNumber возвращаем значение number
+    }
+    public void setNumber(int num) {
+        number = num;               // устанавливаем значение number
+    }
+    public String getFirstName() {
+        return firstName;         // значению переменной getFirstName возвращаем значение firstName
+    }
+    public void setFirstName(String aFirstName)  {
+        firstName = aFirstName;                   // устанавливаем значение aFirstName
+    }
+    public String getLastName() {
+        return lastName;                          // значению переменной getLastName возвращаем значение lastName
+    }
+    public void setLastName(String aLastName)  {
+        lastName = aLastName;}
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String aPhone)  {
+        phone = aPhone; }
+
 }
