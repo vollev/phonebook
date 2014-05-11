7d9b21b86ede6aedbdbc06f4c60cf424cc0d7f7a
vollev <vollev@mail.ru>
2014-05-11 23:12:42 +0400

    Вариант 2

---
+++ src/com/vollev/phonebook/PhoneBook.java
@@ -0,0 +1,13 @@
+package com.vollev.phonebook;
+
+
+/**
+ * Created by vollev on 11.05.2014.
+ */
+public class PhoneBook {
+    public static void main(String[] args) {
+        System.out.print("chupacabra");
+
+
+    }
+}
