201c16771679e3dcc4fbe4c1b53fe772dc914b46
vollev <vollev@mail.ru>
2014-05-11 21:58:38 +0400

    Вариант 1

--- src/com/vollev/phonebook/model/BookEntry/BookEntry.java
+++ src/com/vollev/phonebook/model/BookEntry/BookEntry.java
@@ -1,33 +1,42 @@
-package com.vollev.phonebook;
+package com.vollev.phonebook.model.BookEntry;
 
 /**
  * Created by vollev on 11.05.2014.
  */
-public class PhoneBook { //создаем класс
+public class BookEntry { //создаем класс
     private int id; // объявление целочисленной переменной id
-    private String firstName, lastName, phone ; // объявление класса String и его экземпляров
+    private String firstName, lastName, phone; // объявление класса String и его экземпляров
 
     public int getId() {
         return id;          // значению переменной getId возвращаем значение id
     }
+
     public void setId(int num) {
         id = num;               // устанавливаем значение id
     }
+
     public String getFirstName() {
         return firstName;         // значению переменной getFirstName возвращаем значение firstName
     }
-    public void setFirstName(String aFirstName)  {
+
+    public void setFirstName(String aFirstName) {
         firstName = aFirstName;                   // устанавливаем значение aFirstName
     }
+
     public String getLastName() {
         return lastName;                          // значению переменной getLastName возвращаем значение lastName
     }
-    public void setLastName(String aLastName)  {
-        lastName = aLastName;}
+
+    public void setLastName(String aLastName) {
+        lastName = aLastName;
+    }
+
     public String getPhone() {
         return phone;
     }
-    public void setPhone(String aPhone)  {
-        phone = aPhone; }
+
+    public void setPhone(String aPhone) {
+        phone = aPhone;
+    }
 
 }
