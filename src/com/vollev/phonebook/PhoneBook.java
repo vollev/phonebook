cf09d6d9351a8944898342970e5789678406a85a
vollev <vollev@mail.ru>
2014-05-12 21:59:31 +0400

    Вариант 3

--- src/com/vollev/phonebook/PhoneBook.java
+++ src/com/vollev/phonebook/PhoneBook.java
@@ -1,12 +1,19 @@
 package com.vollev.phonebook;
 
-
+import java.util.Scanner; // импортируем класс
 /**
  * Created by vollev on 11.05.2014.
  */
 public class PhoneBook {
     public static void main(String[] args) {
-        System.out.print("chupacabra");
+       Scanner in = new Scanner(System.in);
+        System.out.println ("Введите имя");
+        String firstName = in.nextLine();
+        System.out.println ("Введите фамилию");
+        String lastName = in.nextLine();
+        System.out.println ("Введите номер телефона");
+        String phone = in.nextLine();
+        System.out.println(firstName, lastName, phone);
 
 
     }
