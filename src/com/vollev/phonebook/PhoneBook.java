2dc9cd264764f38c8c267adefa68a372a3cc9071
vollev <vollev@mail.ru>
2014-05-13 23:49:03 +0400

    Подогнал формат вывода результата, в соответствии с заданием. Повторный запрос в работе/ ps переделал

--- src/com/vollev/phonebook/PhoneBook.java
+++ src/com/vollev/phonebook/PhoneBook.java
@@ -1,5 +1,6 @@
 package com.vollev.phonebook;
 
+
 import java.util.Scanner; // импортируем класс
 /**
  * Created by vollev on 11.05.2014.
@@ -7,14 +8,15 @@ import java.util.Scanner; // импортируем класс
 public class PhoneBook {
     public static void main(String[] args) {
        Scanner in = new Scanner(System.in);
-        System.out.println ("Введите имя");
-        String firstName = in.nextLine();
-        System.out.println ("Введите фамилию");
-        String lastName = in.nextLine();
-        System.out.println ("Введите номер телефона");
-        String phone = in.nextLine();
-        System.out.print (String.format("First name: '%s'\n",firstName));
-        System.out.print (String.format("Last name: '%s'\n", lastName));
-        System.out.print (String.format("Phone: '%s'\n",phone));
+            System.out.println("Введите имя");
+            String firstName = in.nextLine();
+            System.out.println("Введите фамилию");
+            String lastName = in.nextLine();
+            System.out.println("Введите номер телефона");
+            String phone = in.nextLine();
+            System.out.print(String.format("First name: '%s'\nLast name: '%s'\nPhone: '%s'\n", firstName, lastName, phone));
+
     }
+
+
 }
