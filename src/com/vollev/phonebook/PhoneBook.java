9b26ffc6ea2149b0e351eb3ae74158c6741e0586
vollev <vollev@mail.ru>
2014-05-16 20:53:13 +0400

    Подогнал формат вывода результата, в соответствии с заданием. Повторный запрос в работе/ ps переделал

--- src/com/vollev/phonebook/PhoneBook.java
+++ src/com/vollev/phonebook/PhoneBook.java
@@ -1,27 +1,32 @@
 package com.vollev.phonebook;
 
 
-import java.util.Scanner; // импортируем класс
+import java.util.Scanner;
+
 /**
  * Created by vollev on 11.05.2014.
  */
 public class PhoneBook {
-    Boolean uA;
-    public String s1="Y";
-    public static void main(String[] args)  { do {
 
 
-        Scanner in = new Scanner(System.in);
-        System.out.println("Введите имя");
-        String firstName = in.nextLine();
-        System.out.println("Введите фамилию");
-        String lastName = in.nextLine();
-        System.out.println("Введите номер телефона");
-        String phone = in.nextLine();
-        System.out.println(String.format("First name: '%s'\nLast name: '%s'\nPhone: '%s'\n", firstName, lastName, phone));
-        System.out.println("Ещё раз ? Y/N");
-        String userAnswer = in.nextLine();
-    } while (s1.equals.(userAnswer)); }
+    public static void main(String[] args) {
+        String s1 = "Y";
+        Boolean uA;
+        do {
+
+
+            Scanner in = new Scanner(System.in);
+            System.out.println("Введите имя");
+            String firstName = in.nextLine();
+            System.out.println("Введите фамилию");
+            String lastName = in.nextLine();
+            System.out.println("Введите номер телефона");
+            String phone = in.nextLine();
+            System.out.println(String.format("First name: '%s'\nLast name: '%s'\nPhone: '%s'\n", firstName, lastName, phone));
+            System.out.println("Ещё раз ? Y/N");
+            String userAnswer = in.nextLine();
+        } while (s1.equals.(userAnswer));
+    }
 
 
 }
