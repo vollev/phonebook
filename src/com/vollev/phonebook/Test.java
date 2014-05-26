03cdc12a9643d73d7f8de80a14d2aba448471bd0
vollev <vollev@mail.ru>
2014-05-26 21:39:56 +0400

    Подогнал формат вывода результата, в соответствии с заданием. Повторный запрос в работе/ ps переделал

---
+++ src/com/vollev/phonebook/Test.java
@@ -0,0 +1,18 @@
+package com.vollev.phonebook;
+
+import java.util.ArrayList;
+
+/**
+ * Created by vollev on 26.05.2014.
+ */
+public class Test {
+    public static void main(String [] args) {
+        int current = 1 ;
+        for (int a = 0; a < n; a++  ){
+            current *= 2;
+        }
+
+    }
+
+
+}
